13b235f32c22170d5b1bdec3eb1a1eb114ce2313
Asmaelqara <61200944+Asmaelqara@users.noreply.github.com>
2020-09-29 01:17:53 +0200

    Update dbconnection.php

--- admin/dbconnection.php
+++ admin/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "courier_db");
+$con=mysqli_connect("localhost", "asmae_root", "rootroot", "courier_db");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
