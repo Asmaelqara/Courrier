6f004fee17304db987d2b8f59a3e7cbb5988629f
Asmaelqara <61200944+Asmaelqara@users.noreply.github.com>
2020-09-29 13:45:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -45,16 +45,7 @@ require_once('dbconnection.php');
             <h1 class="mb-5">Système de gestion de courriers <br></h1>
           </div>
           <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
-            <form action="search-courier.php" method="post">
-              <div class="form-row">
-                <div class="col-12 col-md-9 mb-2 mb-md-0">
-                  <input name="Consignment" type="text" class="form-control form-control-lg" placeholder="Entrez l'objet du courrier à rechercher...">
-                </div>
-                <div class="col-12 col-md-3">
-                  <button type="submit" class="btn btn-block btn-lg btn-primary">Chercher!</button>
-                </div>
-              </div>
-            </form>
+            
           </div>
         </div>
       </div>
